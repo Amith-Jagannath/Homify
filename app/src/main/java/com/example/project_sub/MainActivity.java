e568c4e172256771480a64a111990fe40dbe05fe
AaronNazareth <4nm20cs005@nmamit.in>
2023-05-07 15:51:12 +0530

    feat:added imp remainder

--- app/src/main/java/com/example/project_sub/MainActivity.java
+++ app/src/main/java/com/example/project_sub/MainActivity.java
@@ -4,18 +4,26 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.app.ActivityCompat;
 
 import com.loopj.android.http.AsyncHttpClient;
@@ -23,6 +31,8 @@ import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 import org.json.JSONObject;
+
+import java.util.Random;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -62,6 +72,40 @@ public class MainActivity extends AppCompatActivity {
         budget = findViewById(R.id.imageButton2);
         track = findViewById(R.id.imageButton4);
         home = findViewById(R.id.imageButton5);
+
+        LinearLayout ll = findViewById(R.id.linearlayout);
+        SQLiteDatabase db = new DataBaseHelper(this).getWritableDatabase();
+        DataBaseHelper dbtest = new DataBaseHelper(this);
+        if(dbtest.isTableExists(db,"Asset")) {
+            Cursor cursor = db.rawQuery("select * from Asset ", null);
+            if (cursor.getCount() == -1) {
+                Toast.makeText(getApplicationContext(), "Assets not found!", Toast.LENGTH_SHORT).show();
+            } else {
+                while (cursor.moveToNext()) {
+                    CardView cv = new CardView(getApplicationContext());
+                    TextView textViewNew = new TextView(this);
+                    textViewNew.setText(cursor.getString(0) + "          " + cursor.getString(1));
+                    textViewNew.setTextSize(20f);
+                    textViewNew.setTypeface(Typeface.DEFAULT_BOLD);
+                    textViewNew.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM);
+                    textViewNew.setPadding(0, 20, 0, 20);
+                    cv.setCardElevation(10f);
+                    cv.setUseCompatPadding(false);
+                    cv.setCardBackgroundColor(Color.WHITE);
+                    cv.setMaxCardElevation(12f);
+                    cv.setPreventCornerOverlap(true);
+                    cv.setBackground(new ColorDrawable(Color.rgb(255,228,125)));
+                    LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT
+                    );
+                    layoutParams.setMargins(0, 16, 0, 16);
+                    cv.setLayoutParams(layoutParams);
+                    cv.addView(textViewNew);
+                    ll.addView(cv);
+                }
+            }
+        }
 
         contact.setOnClickListener(new View.OnClickListener() {
             @Override
